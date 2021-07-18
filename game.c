595b2ed4d20c523dafa22b05b57a2e311856bff7
e6000000000 <skipper224483@gmail.com>
2021-07-18 23:41:14 +0300

    some bugs fixed

--- game.c
+++ game.c
@@ -2,21 +2,27 @@
 
 
 void print_borders() {
-    for (int i = 0; i <= WIDTH; i++) {
+    for (int i = 0; i <= WIDTH + 1; i++) {
         print_square(i, 0, GREY);
-        print_square(i, HEIGHT, GREY);
+        print_square(i, HEIGHT+1, GREY);
     }
-    for (int i = 0; i <= HEIGHT; i++) {
+    for (int i = 0; i <= HEIGHT + 1; i++) {
         print_square(0, i, GREY);
-        print_square(WIDTH, i, GREY);
+        print_square(WIDTH+1, i, GREY);
     }
 }
 
-void restart(struct Point *player, enum direction *dir) {
+void respawn_apple(struct Point *apple) {
+    (*apple).x = 1 + rand() % WIDTH;
+    (*apple).y = 1 + rand() % HEIGHT;
+}
+
+void restart(struct Point *player, enum direction *dir, struct Point *apple, int *score) {
     clear_terminal();
     hide_cursor();
     print_borders();
     *dir = Right;
+    *score = 0;
 
     int max_player_length = WIDTH * HEIGHT;
     for (int i = 0; i < max_player_length+1; i++) {
@@ -25,15 +31,22 @@ void restart(struct Point *player, enum direction *dir) {
     }
     (*player).x = WIDTH / 2;
     (*player).y = HEIGHT / 2;
+
+    respawn_apple(apple);
 }
+
 
 bool is_collised_with_wals(struct Point *player) {
     if ((*player).x < 1) return true;
-    if ((*player).x >= WIDTH) return true;
+    if ((*player).x > WIDTH) return true;
     if ((*player).y < 1) return true;
-    if ((*player).y >= HEIGHT) return true;
+    if ((*player).y > HEIGHT) return true;
 
     return false;
+}
+
+bool is_collised_with_apple(struct Point *player, struct Point *apple) {
+    return (*player).x == (*apple).x && (*player).y == (*apple).y;
 }
 
 void move_by_direction(struct Point *player, enum direction *dir) {
@@ -51,7 +64,7 @@ void move_by_direction(struct Point *player, enum direction *dir) {
     }
 }
 
-void update_tail(struct Point *player, int *score) {
+void prepare_player_to_move(struct Point *player, int *score) {
     int player_length = SNAKE_START_LEGTH + *score;
     for (int i = player_length; i > 0; i--) {
         player[i].x = player[i - 1].x;
@@ -63,7 +76,7 @@ bool is_point_inside_gamefield(struct Point *point) {
     return (*point).x > 0 && (*point).y > 0 && (*point).x <= WIDTH && (*point).y <= HEIGHT;
 }
 
-void print_player(struct Point *player, int *score) {
+void draw_changes(struct Point *player, struct Point *apple, int *score) {
     int player_length = SNAKE_START_LEGTH + *score;
 
     if (is_point_inside_gamefield(&player[player_length]))
@@ -72,24 +85,33 @@ void print_player(struct Point *player, int *score) {
         if (is_point_inside_gamefield(&player[i]))
             print_square(player[i].x, player[i].y, GREEN);
     }
+
+    if (is_point_inside_gamefield(apple))
+        print_square((*apple).x, (*apple).y, RED);
 }
 
 void* update_game(void *_dir) {
+    srand(time(0));
     struct Point *player = 0;
     int max_player_length = WIDTH * HEIGHT;
     player = malloc((max_player_length+1) * sizeof(struct Point));
     enum direction *dir = (enum direction*)_dir;
+    struct Point apple;
     int score = 0;
 
-    restart(player, dir);
+    restart(player, dir, &apple, &score);
 
     while (1) {
         print_square((*player).x, (*player).y, GREEN);
-        update_tail(player, &score);
+        prepare_player_to_move(player, &score);
         move_by_direction(player, dir);
-        print_player(player, &score);
+        draw_changes(player, &apple, &score);
         if (is_collised_with_wals(player))
-            restart(player, dir);
+            restart(player, dir, &apple, &score);
+        if (is_collised_with_apple(player, &apple)) {
+            score += 1;
+            respawn_apple(&apple);
+        }
 
         usleep(1000000/FPS);
     }
--- game.h
+++ game.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include "input.h"
 #include "terminal.h"
